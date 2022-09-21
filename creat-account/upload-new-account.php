2337334c18da083035d50e101a82df59cfef3bad
lukaserikss0n <lukas.emil.eriksson@outlook.com>
2022-09-21 21:45:24 +0200

    buggs fixed

--- creat-account/upload-new-account.php
+++ creat-account/upload-new-account.php
@@ -1,14 +1,15 @@
 <?php
-include "server-connect.php";
+include "../server-connect.php";
 
 if(isset($_POST['submit'])){
-    header("location: index.php");
 
     $creat_username = $_POST['creatusername'];
     $creat_password = $_POST['creatpassword'];
 
     $insert_account = "insert into account (username, user_password) values('$creat_username', '$creat_password')";
     $append_account = mysqli_query($con, $insert_account);
+    header("location: ../index.php");
+
 }
 
 
