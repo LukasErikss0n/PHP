6b17be30b5395ef3ca5c13f28c6971f561ce0384
lukaserikss0n <lukas.emil.eriksson@outlook.com>
2022-09-26 13:06:59 +0200

    check uppload criterias

--- check-account.php
+++ check-account.php
@@ -18,15 +18,18 @@ if(isset($_POST['submit'])){
         $id = $obj['id'];
         $password = $obj['user_password'];
 
-        if($name === $input_name || $password === $input_password){
+        if($name === $input_name && $password === $input_password){
             session_start();
             $_SESSION["username"] = $input_name;
             header("location: logdin.php");
 
+        }else{
+            echo "no account found by the name " . $input_name. " pleas try again ";
+            echo "<a href = 'index.php'>go back</a>";
         }
          
     } else {
-        echo "no account found by the name " . $input_name. " pleas try again ";
+        echo "no account found, incorrect password or username";
         echo "<a href = 'index.php'>go back</a>";
     }
 }
--- file-upload.php
+++ file-upload.php
@@ -1,6 +1,44 @@
 <?php
-//session_start();
- //if (isset($_SESSION["username"])) {
- //   echo "success uploading the file";
-// }
+session_start();
+ if (isset($_SESSION["username"])) {
+    if(isset($_POST['submit'])){
+        $file = $_FILES['file'];
+
+        $fileName = $_FILES['file']['name'];
+        $fileTempName = $_FILES['file']['tmp_name'];
+        $fileSize = $_FILES['file']['size'];
+        $fileError = $_FILES['file']['error'];
+        $fileType = $_FILES['file']['type'];
+
+        $fileExtensionUpper = explode('.', $fileName);
+        $fileActualExtensionLower = strtolower(end($fileExtensionUpper));
+
+        $allowed = array ('jpg');
+
+        if (in_array($fileActualExtensionLower, $allowed)) {
+            if ($fileError === 0) {
+                if ($fileSize < 5000000) {
+                    $fileDestination = 'uploads/ ' . $fileName;
+                    move_uploaded_file($fileTempName, $fileDestination);
+                    header("location: logdin.php?upploadsuccess");
+
+
+                } else {
+                    echo "your file is to big";
+                }
+            } else {
+                echo "there was an error uploading your file";
+            }
+        }else {
+            echo "you cannot upload files of this type";
+        }
+
+    }
+} else{
+    echo "please login before you uppload";
+}
+
+
+
+//move_uploaded_file($_FILES)
 
