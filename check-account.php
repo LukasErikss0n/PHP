789ed06e27c7e635959312afa996b1aed91b79f0
lukaserikss0n <lukas.eriksson@skola.taby.se>
2022-10-10 10:36:49 +0200

    hash

--- check-account.php
+++ check-account.php
@@ -4,21 +4,29 @@ include "server-connect.php";
 if(isset($_POST['submit'])){
     $input_name = $_POST['username'];
     $input_password = $_POST['password'];
-    if($input_name === "" or $input_password === ""){
-        $input_name = "incorrect";
-        $input_password = "nopasswordfound";
-    }
+    
 
-    $user_info = "SELECT id, username, user_password from account where username = '$input_name'";
-    $result = $con->query($user_info);
+    $stmt = $con->prepare("SELECT id, username, user_password from account where username = ?");
+    $stmt->bind_param("s", $input_name);
+    $stmt->execute();
+    $result = $stmt->get_result();
+   // var_dump($result);
+    //$result = $con->query($user_info);
 
     if (mysqli_num_rows($result) > 0) {
+        $h = password_hash($input_password, PASSWORD_DEFAULT);
+        echo $input_password;
+        echo "<br>";
+        echo $h;
+        echo "<br>";
         $obj = mysqli_fetch_assoc($result);
+        //var_dump($obj);
         $name = $obj['username'];
         $id = $obj['id'];
-        $password = $obj['user_password'];
+        $hash = $obj['user_password'];
+        var_dump(password_verify('$input_password', $hash));
 
-        if($name === $input_name && $password === $input_password){
+        if(password_verify($input_password, $hash)){
             session_start();
             $_SESSION["username"] = $input_name;
             header("location:logdin.php");
--- creat-account/upload-new-account.php
+++ creat-account/upload-new-account.php
@@ -6,7 +6,9 @@ if(isset($_POST['submit'])){
     $creat_username = $_POST['creatusername'];
     $creat_password = $_POST['creatpassword'];
 
-    $insert_account = "insert into account (username, user_password) values('$creat_username', '$creat_password')";
+    $hash_password = password_hash($creat_password, PASSWORD_DEFAULT);//: string
+
+    $insert_account = "insert into account (username, user_password) values('$creat_username', '$hash_password')";
     $append_account = mysqli_query($con, $insert_account);
     header("location: ../index.php");
 
